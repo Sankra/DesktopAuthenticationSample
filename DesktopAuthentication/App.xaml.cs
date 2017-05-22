c11073fae749217a863dd50f0f5215f8026d0415
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-05-22 14:49:38 +0200

    Added sample code

---
+++ DesktopAuthentication/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Identity.Client;
+
+namespace DesktopAuthentication {
+    public partial class App : Application {
+        //Below is the clientId of your app registration. 
+        //You have to replace the below with the Application Id for your app registration
+        private static string ClientId = "50dff152-0882-4fc8-a8c9-1e77b132c3ff";
+
+        public static PublicClientApplication PublicClientApp = new PublicClientApplication(ClientId);
+    }
+}
